fc03297d9d6426eef1b1d43eb3d2c8f9490d2d71
Jose-souza-dev <franciscojosemusic131@gmail.com>
2023-08-05 11:54:55 -0300

    Criação de qrcode image

---
+++ js/script.js
@@ -0,0 +1,9 @@
+function abrirFechar(){
+    let qrc = document.getElementById('qrc')
+    let btn = document.getElementById('btn')
+    if(qrc.style.display =='block'){
+        qrc.style.display ='none'
+    }else{
+        qrc.style.display ='block'
+    }
+}
