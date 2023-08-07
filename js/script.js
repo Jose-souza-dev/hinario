cd2bd6fb96596e4a6aacc9200d5d1e558f946d01
Jose-souza-dev <franciscojosemusic131@gmail.com>
2023-08-07 16:30:30 -0300

    Melhorias no css do para telas grandes

--- js/script.js
+++ js/script.js
@@ -1,3 +1,16 @@
+
+        // Função para verificar o tamanho da tag body e exibir o aviso, se necessário
+        function aviso() {
+            const bodyWidth = document.body.clientWidth;
+            if (bodyWidth > 400) {
+                alert("Este site se comporta melhor em telas de celular. Por favor, redimensione a janela do navegador para obter uma experiência ideal.");
+            }
+        }
+
+        // Chama a função para verificar o tamanho da tag body e exibir o aviso, se necessário
+        aviso();
+
+
 function abrirFechar(){
     let qrc = document.getElementById('qrc')
     let btn = document.getElementById('btn')
