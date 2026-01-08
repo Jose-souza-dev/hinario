3e4e8951ea7c8269d16ed75e9bc0bf7dada892be
Francisco José Ferreira de Souza <129606602+Jose-souza-dev@users.noreply.github.com>
2026-01-08 10:50:37 -0300

    Update de todas as páginas colocando script

--- js/script.js
+++ js/script.js
@@ -1,29 +1,8 @@
+// função que muda o title do site
+    let titulo = document.querySelector("title");
+    titulo.innerText = "Hino Flow";
 
-        // Função para verificar o tamanho da tag body e exibir o aviso, se necessário
-        function aviso() {
-            const bodyWidth = document.body.clientWidth;
-            if (bodyWidth > 400) {
-                alert("Este site se comporta melhor em telas de celular. Por favor, redimensione a janela do navegador para obter uma experiência ideal.");
-            }
-        }
-
-        // Chama a função para verificar o tamanho da tag body e exibir o aviso, se necessário
-        //aviso();
-
-
-        function abrirFechar(){
-            const menu = document.getElementById('menu');
-
-            if(menu.style.display ==='block'){
-                menu.style.display ='none';
-                
-            }else{
-            menu.style.display ='block';
-            
-            }
-        }
-
-        // Função de declaração tradicional para realizar a busca/filtro
+// Função de declaração tradicional para realizar a busca/filtro
         function buscar() {
             // 1. Pega o elemento input de busca
             const input = document.querySelector("#search");
